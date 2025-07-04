d11fcfef9be4958563c323466104a57521e52570
Gabriel <gabrielgonzalezlisboa@gmail.com>
2025-07-04 17:55:51 -0400

    .

--- scapy/sniffer.py
+++ scapy/sniffer.py
@@ -1,14 +1,15 @@
 from scapy.all import sniff, wrpcap, get_if_list
-from termcolor import cprint
 
 OUTPUT = "/captures/trace.pcap"
 
+# Lista interfaces disponibles
 interfaces = get_if_list()
-cprint("Interfaces detectadas en este contenedor:", "cyan", attrs=["bold"])
+print("Interfaces detectadas en este contenedor:")
 for i, iface in enumerate(interfaces):
-    cprint(f"  [{i+1}] {iface}", "yellow")
+    print(f"  [{i+1}] {iface}")
 
-input_str = input(f"\n¿Elige la interfaz por nombre o número (ENTER para 'eth0')? ").strip()
+# Pregunta interfaz (nombre o número)
+input_str = input("\nElige la interfaz por nombre o número (ENTER para 'eth0'): ").strip()
 if input_str == "":
     IFACE = "eth0"
 elif input_str.isdigit() and 1 <= int(input_str) <= len(interfaces):
@@ -17,8 +18,8 @@ else:
     IFACE = input_str
 
 FILTER = "tcp port 5672"
-cprint(f"\nSniffing interface: {IFACE} | filter: {FILTER}\n", "cyan")
+print(f"\nSniffing interface: {IFACE} | filter: {FILTER}")
 
 pkts = sniff(filter=FILTER, iface=IFACE, timeout=60)
 wrpcap(OUTPUT, pkts)
-cprint(f"Saved {len(pkts)} packets to {OUTPUT}", "green", attrs=["bold"])
+print(f"Guardado {len(pkts)} paquetes en {OUTPUT}")
