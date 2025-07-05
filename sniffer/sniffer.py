26aefa5ae4b72af624f0fd5c273d796ab29bca02
Gabriel <gabrielgonzalezlisboa@gmail.com>
2025-07-04 21:47:29 -0400

    Scripts

---
+++ sniffer/sniffer.py
@@ -0,0 +1,22 @@
+from scapy.all import sniff, TCP, Raw, wrpcap
+
+capturados = []
+
+def mostrar_paquete(pkt):
+    if pkt.haslayer(Raw):
+        data = pkt[Raw].load
+        try:
+            texto = data.decode(errors="ignore")
+            texto_limpio = texto.strip()
+            if texto_limpio:
+                print(texto_limpio)
+        except Exception:
+            pass
+        capturados.append(pkt)
+
+print("Sniffeando paquetes AMQP en TCP 5672... Solo texto del mensaje.")
+sniff(filter="tcp port 5672", prn=mostrar_paquete, store=0, timeout=60)
+
+# Guarda todo en un pcap al terminar
+wrpcap("captura_amqp.pcap", capturados)
+print("Captura guardada en captura_amqp.pcap")
